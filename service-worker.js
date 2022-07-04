5e0d8ec8bb3bd872f0bfb1e2d42befa86994e5ea
adelaweidischova <48671822+adelaweidischova@users.noreply.github.com>
2022-07-04 13:20:20 +0000

    Deploying to docs from @ books-are-next/dante-alighieri-to-sladke-jmeno-beatrice@65b72b9d39a0c229b77208c26fecb21d5ae14a5b 🚀

--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-13bc1b8';
+const CACHE = 'cache-65b72b9';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
